77719ce7c11fa44ce8c5f2a30d2945046c308b50
Shriyansh Bhargava <63575417+shriyanshbhargava@users.noreply.github.com>
2024-06-10 07:16:21 +0000

    removed switch and routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import "./styles.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import BookSearch from "./Components/BookSearch";
 import Bookshelf from "./Components/Bookshelf";
 
 export default function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={BookSearch} />
-          <Route path="/bookshelf" component={Bookshelf} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <BookSearch />
+      <Bookshelf />
+    </div>
   );
 }
